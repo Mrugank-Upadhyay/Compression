ce4f0eb9bdecdc38e33faca05477ef19115c161c
Mrugank <mrugank2@gmail.com>
2021-04-14 07:20:29 -0400

    completed encoding algorithm

--- encode.cpp
+++ encode.cpp
@@ -18,27 +18,33 @@ struct Node{
     Node(unsigned char c, int f, node_pointer l = nullptr, node_pointer r = nullptr) : character(c), left(l), right(r), freq(f) {}
 };
 
-
+// create the frequency array
 void makeFreqArray(string text, vector<int> & alphabetFreq) {
     int len = text.length();
+    // loop through text, increment at each character index
     for(int i = 0; i < len; i++) {
         unsigned char c = text[i];
         alphabetFreq[c]++;
     }
 }
 
+// performs modulus on type double
 double mod(double x, double y) {
     return x - (int)(x/y) * y;
 }
 
+// performs floor on type double
 double floor(double n) {
     return n - (mod(n, 1) >= 0 ? mod(n, 1) : (1 + mod(n, 1)));
 }
 
+// Performs fix-up for minHeap
+// algorithm as in Module 2
 void fixUp(vector<node_pointer> & minHeap) {
     double k = minHeap.size() - 1;
     double parentK = floor((k - 1)/2);
 
+    // while parent exists and minHeap[parent] < minHeap[k]
     while (( parentK >= 0) && (minHeap[parentK]->freq > minHeap[k]->freq)) {
         auto tmp = minHeap[k];
         minHeap[k] = minHeap[parentK];
@@ -48,21 +54,29 @@ void fixUp(vector<node_pointer> & minHeap) {
     }
 }
 
+// Inserts a single node into the heap
 void insertHeap(vector<node_pointer> & minHeap, node_pointer node) {
     minHeap.push_back(node);
     fixUp(minHeap);
 }
 
+// Inserts the two popped tries, T1 and T2 into the heap by creating a new combined trie
 void insertHeap(vector<node_pointer> & minHeap, node_pointer T1, node_pointer T2) {
     node_pointer Trie = new Node(char(0), T1->freq + T2->freq, T1, T2);
     insertHeap(minHeap, Trie);
 }
 
+// Performs fixDown on heap
+// algorithm as in Module 2
 void fixDown(vector<node_pointer> & minHeap) {
     int i = 0;
     int size = minHeap.size();
+    // while last(i) is less than size
     while (2*i + 1 < size) {
+        // j equals left child
         int j = 2*i + 1;
+
+        // if j is within the heap and minHeap at j+1 is less than j, increment j
         if ((j != size - 1) && (minHeap[j + 1]->freq < minHeap[j]->freq)) {
             j++;
         }
@@ -77,8 +91,11 @@ void fixDown(vector<node_pointer> & minHeap) {
     }
 }
 
+// Deletes the minimum value in heap
+// algorithm as in Module 2
 node_pointer deleteMin(vector<node_pointer> & minHeap) {
     auto tmp = minHeap[0];
+    // swap with last leaf
     minHeap[0] = minHeap[minHeap.size() - 1];
     minHeap[minHeap.size() - 1] = tmp;
 
@@ -90,13 +107,18 @@ node_pointer deleteMin(vector<node_pointer> & minHeap) {
     return minNode;
 }
 
+// Initialize the minHeap
 vector<node_pointer> makeInitHeap(vector<int> & alphabetFreq) {
     vector<node_pointer> minHeap = vector<node_pointer>();
     int len = alphabetFreq.size();
+
     for (int i = 0; i < len; i++) {
+        // skip characters not in text
         if (alphabetFreq[i] == 0) {
             continue;
         }
+
+        // make a trie for the character
         node_pointer node = new Node(char(i), alphabetFreq[i], nullptr, nullptr);
         insertHeap(minHeap, node);
     }
@@ -104,8 +126,9 @@ vector<node_pointer> makeInitHeap(vector<int> & alphabetFreq) {
     return minHeap;
 }
 
-
+// Print the character and depth as c_i d_i
 void printTrie(node_pointer finalTrie, int depth, vector<string> & alphabetEncode, string encode = "") {
+    // if not a character
     if (finalTrie->character == char(0)) {
         depth++;
         if (finalTrie->left != nullptr) {
@@ -135,15 +158,16 @@ int main(){
     makeFreqArray(text, alphabetFreq);
     vector<node_pointer> minHeap = makeInitHeap(alphabetFreq);
 
+    // Perform repeated removal of minimum tries and insertions to get one final trie
     while (minHeap.size() > 1) {
         node_pointer T1 = deleteMin(minHeap);
         node_pointer T2 = deleteMin(minHeap);
         insertHeap(minHeap, T1, T2);
     }
 
-
     auto finalTrie = deleteMin(minHeap);
 
+    // contains the encoding for each character
     vector<string> alphabetEncode = vector<string>(256, "");
     // Prints c_i d_i pairs
     printTrie(finalTrie, 0, alphabetEncode);
