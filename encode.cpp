ecdc48bcf8d9b8c8b9e2d1b939a68fc828cb5123
Mrugank <mrugank2@gmail.com>
2021-04-13 20:23:34 -0400

    completed huffman trie algorithm

--- encode.cpp
+++ encode.cpp
@@ -15,20 +15,19 @@ struct Node{
     unsigned char character;
     int freq;
     node_pointer parent;
+    string trieName;
     
     Node() {}
-    Node(unsigned char c, int f, node_pointer l = nullptr, node_pointer r = nullptr) : character(c), left(l), right(r), freq(f) {}
+    Node(unsigned char c, int f, node_pointer l = nullptr, node_pointer r = nullptr, string str = "") : character(c), left(l), right(r), freq(f), trieName(str) {}
     void addParent(node_pointer parentNode) {
         parent = parentNode;
     }
 };
 
-int comp(node_pointer a, node_pointer b){
-    return -(a->freq - b->freq);
-}
+// int comp(node_pointer a, node_pointer b){
+//     return -(a->freq - b->freq);
+// }
 
-
-// add your code here
 
 void makeFreqArray(string text, vector<int> & alphabetFreq) {
     int len = text.length();
@@ -38,38 +37,64 @@ void makeFreqArray(string text, vector<int> & alphabetFreq) {
     }
 }
 
+double mod(double x, double y) {
+    return x - (int)(x/y) * y;
+}
 
-void fixUp(vector<node_pointer> minHeap) {
-    int i = minHeap.size() - 1;
-    int parentK = (i - 1)/2;
-    while (( parentK >= 0) && (minHeap[parentK] < minHeap[i])) {
+double floor(double n) {
+    return n - (mod(n, 1) >= 0 ? mod(n, 1) : (1 + mod(n, 1)));
+}
+
+void fixUp(vector<node_pointer> & minHeap) {
+    double i = minHeap.size() - 1;
+    double parentK = floor((i - 1)/2);
+
+    cout << "START ----> i: " << i << " parentK: " << parentK << endl;
+    for (int i = 0; i < minHeap.size(); i++) {
+        cout << minHeap[i]->trieName << ":" << minHeap[i]->freq << endl;
+    }
+
+    while (( parentK >= 0) && (minHeap[parentK]->freq > minHeap[i]->freq)) {
         auto tmp = minHeap[i];
         minHeap[i] = minHeap[parentK];
         minHeap[parentK] = tmp;
         i = parentK;
-        parentK = (i - 1)/2;
+        parentK = floor((i - 1)/2);
+
+        cout << "LOOP ---> i: " << i << " parentK: " << parentK << endl;
+
+        for (int i = 0; i < minHeap.size(); i++) {
+        cout << minHeap[i]->trieName << ":" << minHeap[i]->freq << endl;
+        }
+    }
+    
+    cout << "END" << endl;
+    for (int i = 0; i < minHeap.size(); i++) {
+        cout << minHeap[i]->trieName << ":" << minHeap[i]->freq << endl;
     }
 }
 
-void insertHeap(vector<node_pointer> minHeap, node_pointer node) {
+void insertHeap(vector<node_pointer> & minHeap, node_pointer node) {
     minHeap.push_back(node);
+    //cout << "minHeap size: " << minHeap.size() << endl;
     fixUp(minHeap);
 }
 
-void insertHeap(vector<node_pointer> minHeap, node_pointer T1, node_pointer T2) {
-    node_pointer Trie = new Node(char(0), T1->freq + T2->freq, T1, T2);
+void insertHeap(vector<node_pointer> & minHeap, node_pointer T1, node_pointer T2) {
+    cout << "triename = " << T1->trieName + T2->trieName << endl;
+    node_pointer Trie = new Node(char(0), T1->freq + T2->freq, T1, T2, T1->trieName + T2->trieName);
     insertHeap(minHeap, Trie);
 }
 
-void fixDown(vector<node_pointer> minHeap) {
+void fixDown(vector<node_pointer> & minHeap) {
     int i = 0;
     int size = minHeap.size();
     while (2*i + 1 < size) {
         int j = 2*i + 1;
-        if ((j != size - 1) && (minHeap[j + 1] > minHeap[j])) {
+        if ((j != size - 1) && (minHeap[j + 1]->freq < minHeap[j]->freq)) {
             j++;
         }
-        if (minHeap[i] >= minHeap[j]) {
+        if (minHeap[i]->freq <= minHeap[j]->freq) {
             break;
         }
 
@@ -80,7 +105,7 @@ void fixDown(vector<node_pointer> minHeap) {
     }
 }
 
-node_pointer deleteMin(vector<node_pointer> minHeap) {
+node_pointer deleteMin(vector<node_pointer> & minHeap) {
     auto tmp = minHeap[0];
     minHeap[0] = minHeap[minHeap.size() - 1];
     minHeap[minHeap.size() - 1] = tmp;
@@ -100,11 +125,30 @@ vector<node_pointer> makeInitHeap(vector<int> & alphabetFreq) {
         if (alphabetFreq[i] == 0) {
             continue;
         }
-        node_pointer node = new Node(char(i), alphabetFreq[i]);
+        node_pointer node = new Node(char(i), alphabetFreq[i], nullptr, nullptr, string(1,char(i)));
         insertHeap(minHeap, node);
     }
+
+    return minHeap;
 }
 
+
+void printTrie(node_pointer finalTrie) {
+    if (finalTrie->character == char(0)) {
+        cout << "*" << endl;
+        if (finalTrie->left != nullptr) {
+            printTrie(finalTrie->left);
+        }
+        if (finalTrie->right != nullptr) {
+            printTrie(finalTrie->right);
+        }
+    }
+
+    else {
+        cout << finalTrie->character << " ";
+    }
+    
+}
 
 //----------------------------------------------------//
 // main                                               //
@@ -121,13 +165,35 @@ int main(){
     //     }
     // }
 
+    // cout << endl;
+    cout << "minHeap:" << endl;
+    for (int i = 0; i < minHeap.size(); i++) {
+        cout << minHeap[i]->trieName << ": " << minHeap[i]->freq << ", ";
+    }
+    cout << endl;
+
     while (minHeap.size() > 1) {
         node_pointer T1 = deleteMin(minHeap);
         node_pointer T2 = deleteMin(minHeap);
         insertHeap(minHeap, T1, T2);
+       
+        for (int i = 0; i < minHeap.size(); i++) {
+            cout << minHeap[i]->trieName << ": " << minHeap[i]->freq << ", ";
+        }
+        cout << endl;
     }
 
-    auto finalTrie = deleteMin(minHeap);
+    for (int i = 0; i < minHeap.size(); i++) {
+        cout << minHeap[i]->trieName << ": " << minHeap[i]->freq << ", ";
+    }
+    cout << endl;
 
+    auto finalTrie = deleteMin(minHeap);
+    for (int i = 0; i < minHeap.size(); i++) {
+        cout << minHeap[i]->trieName << ": " << minHeap[i]->freq << ", ";
+    }
+    cout << endl;
+
+    printTrie(finalTrie);
     // print out the encodings and coded text
 }
